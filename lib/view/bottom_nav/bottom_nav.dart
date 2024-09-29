372f1b1208dfbd000066436ca0cd181198db0b18
AthiraaAthi <athiraa2612@gmail.com>
2024-09-29 18:30:20 +0530

    wrapped the navigationbar with container

--- lib/view/bottom_nav/bottom_nav.dart
+++ lib/view/bottom_nav/bottom_nav.dart
@@ -20,29 +20,44 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       body: screens[_selectedIndex],
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30.0),
-          child: BottomNavigationBar(
-              unselectedItemColor: Colors.grey,
-              selectedItemColor: Colors.white,
-              backgroundColor: Colors.black,
-              onTap: (index) {
-                _selectedIndex = index;
-                setState(() {});
-              },
-              currentIndex: _selectedIndex,
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: "Home"), //items always have to greater than 3
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.card_giftcard), label: "Search"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_cart), label: "ComingSoon"),
-              ]),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30.0), // Rounded edges
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black
+                    .withOpacity(0.3), // Shadow to create floating effect
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: Offset(0, 3), // Positioned shadow under the bar
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: BottomNavigationBar(
+                unselectedItemColor: Colors.grey,
+                selectedItemColor: Colors.white,
+                backgroundColor: Colors.black,
+                onTap: (index) {
+                  _selectedIndex = index;
+                  setState(() {});
+                },
+                currentIndex: _selectedIndex,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.card_giftcard), label: "Search"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.shopping_cart), label: "ComingSoon"),
+                ]),
+          ),
         ),
       ),
     );
