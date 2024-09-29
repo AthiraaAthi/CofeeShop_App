9ce9f63b2027ad94d10dfecf2a2865575623869d
AthiraaAthi <athiraa2612@gmail.com>
2024-09-29 12:12:13 +0530

    added special font to splash screen

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cofee_ui/utils/image_constant/img_constant.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -9,7 +10,20 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(ImageConstant.cofee))),
+            image: DecorationImage(
+                image: AssetImage(ImageConstant.cofee), fit: BoxFit.cover)),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, bottom: 200),
+            child: Text(
+              "Magic Coffee",
+              style: GoogleFonts.dancingScript(
+                fontSize: 45,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
