300b9ba7a35f47103472d5bf57bf0ef9928b2ec6
AthiraaAthi <athiraa2612@gmail.com>
2024-09-30 18:21:50 +0530

    added one more color to color constant

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorConstant {
   static Color mainBlue = Color(0xff324A59);
   static Color White = Color(0xffF7F8FB);
+  static Color lightPurple = Color(0xffC58BF2);
 }
