38e57196e36fe15a9cadc1a0a5d3682e50f57327
AthiraaAthi <athiraa2612@gmail.com>
2024-09-30 18:41:12 +0530

    added appbar with icons and texts

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorConstant {
   static Color mainBlue = Color(0xff324A59);
-  static Color White = Color(0xffF7F8FB);
+  static Color White = Color(0xffE5E5E5);
   static Color lightPurple = Color(0xffC58BF2);
 }
--- lib/view/home_screen/home.dart
+++ lib/view/home_screen/home.dart
@@ -1,3 +1,5 @@
+import 'package:cofee_ui/utils/color_constant/color_constant.dart';
+import 'package:cofee_ui/utils/image_constant/img_constant.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,15 +8,52 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("welcome"),
-        leading: Row(
+        backgroundColor: Colors.white,
+        toolbarHeight: 100,
+        title: Column(
           children: [
-            InkWell(
-                //child: Image.asset(),
-                )
+            Text(
+              "welcome!",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  fontSize: 20),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, right: 20),
+              child: Text(
+                "Alex",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
           ],
         ),
+        actions: [
+          Row(
+            children: [
+              Container(
+                height: 26,
+                width: 26,
+                child: InkWell(
+                  child: Image.asset(ImageConstant.cart),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Container(
+                height: 26,
+                width: 26,
+                child: InkWell(
+                  child: Image.asset(ImageConstant.profile),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
