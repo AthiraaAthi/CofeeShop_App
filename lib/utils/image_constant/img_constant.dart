b8a4180596cf340212500996f3064fc9a2e4be66
AthiraaAthi <athiraa2612@gmail.com>
2024-09-30 18:30:39 +0530

    changed the slash to the right one to access

--- lib/utils/image_constant/img_constant.dart
+++ lib/utils/image_constant/img_constant.dart
@@ -1,11 +1,11 @@
 class ImageConstant {
   static String cofee = "assets/images/coffee.jpeg";
-  static String cart = "assets\images\cart.png";
-  static String profile = "assets\images\profile.png";
-  static String americano = "assets\images\Americano.png";
-  static String cappuccino = "assets\images\cappucino.png";
-  static String latte = "assets\images\Latte.png";
-  static String flatWhite = "assets\images\Flat white.png";
-  static String raf = "assets\images\raf.png";
-  static String espresso = "assets\images\Espresso.png";
+  static String cart = "assets/images/cart.png";
+  static String profile = "assets/images/profile.png";
+  static String americano = "assets/images/Americano.png";
+  static String cappuccino = "assets/images/cappucino.png";
+  static String latte = "assets/images/Latte.png";
+  static String flatWhite = "assets/images/Flat white.png";
+  static String raf = "assets/images/raf.png";
+  static String espresso = "assets/images/Espresso.png";
 }
