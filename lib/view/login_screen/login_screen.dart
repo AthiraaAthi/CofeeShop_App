f8e6ed145562e79a16d0c62bd9415b0fb3e33a74
AthiraaAthi <athiraa2612@gmail.com>
2024-10-09 19:14:50 +0530

    added mail field

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cofee_ui/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -10,10 +11,44 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Let's Get You In",
               style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome Pal",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17,
+                  color: ColorConstant.nameGrey),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 0.7)),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(
+                        left: 80, top: 10, bottom: 10, right: 20),
+                    border: InputBorder.none,
+                    hintText: "Email Address",
+                    hintStyle:
+                        TextStyle(fontSize: 13, color: Colors.grey.shade400),
+                    prefixIcon: Icon(
+                      Icons.mail_outlined,
+                    )),
+              ),
             )
           ],
         ),
