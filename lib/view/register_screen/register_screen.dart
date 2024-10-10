5779f17aa390db27e47aabb821806429fc496fed
AthiraaAthi <athiraa2612@gmail.com>
2024-10-10 15:22:55 +0530

    added name field

--- lib/view/register_screen/register_screen.dart
+++ lib/view/register_screen/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cofee_ui/utils/color_constant/color_constant.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -7,6 +8,51 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Sign Up",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Create an account here",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17,
+                  color: ColorConstant.nameGrey),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 0.7)),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                    border: InputBorder.none,
+                    hintText: "Name",
+                    hintStyle:
+                        TextStyle(fontSize: 15, color: Colors.grey.shade400),
+                    prefixIcon: Icon(
+                      Icons.person_2_outlined,
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
