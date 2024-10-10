84e3d5dab492d50aed80f48fc006c3e061b23e1b
AthiraaAthi <athiraa2612@gmail.com>
2024-10-10 15:25:48 +0530

    added password field

--- lib/view/register_screen/register_screen.dart
+++ lib/view/register_screen/register_screen.dart
@@ -94,6 +94,28 @@ class RegisterScreen extends StatelessWidget {
                     )),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 0.7)),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    hintStyle:
+                        TextStyle(fontSize: 15, color: Colors.grey.shade400),
+                    prefixIcon: Icon(
+                      Icons.lock_outline,
+                    )),
+              ),
+            ),
           ],
         ),
       ),
