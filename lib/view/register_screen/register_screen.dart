2586d530e389faac0ccb29c0cf0636275297ac8a
AthiraaAthi <athiraa2612@gmail.com>
2024-10-10 15:24:11 +0530

    added mobile number field

--- lib/view/register_screen/register_screen.dart
+++ lib/view/register_screen/register_screen.dart
@@ -50,6 +50,28 @@ class RegisterScreen extends StatelessWidget {
                     )),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade400, width: 0.7)),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                    border: InputBorder.none,
+                    hintText: "Mobile Number",
+                    hintStyle:
+                        TextStyle(fontSize: 15, color: Colors.grey.shade400),
+                    prefixIcon: Icon(
+                      Icons.phone_android,
+                    )),
+              ),
+            ),
           ],
         ),
       ),
