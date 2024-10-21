5557e344f8d131084dd8bef5a55eee5127169a42
AthiraaAthi <athiraa2612@gmail.com>
2024-10-21 18:02:57 +0530

    added scrollview on order screen

--- lib/view/order_screen/order_screen.dart
+++ lib/view/order_screen/order_screen.dart
@@ -29,250 +29,252 @@ class OrderScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w400),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Color(0xffF7F8FB),
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                      image: AssetImage(ImageConstant.cappuccino))),
-              height: 146,
-              width: 340,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Capuccino",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                Container(
-                  height: 30,
-                  width: 120,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(
-                        color: ColorConstant.grey,
-                      )),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.remove,
-                            size: 15,
-                          )),
-                      Text("1"),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.add,
-                            size: 15,
-                          )),
-                    ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xffF7F8FB),
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage(ImageConstant.cappuccino))),
+                height: 146,
+                width: 340,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Capuccino",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: ColorConstant.grey,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Ristretto",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 80,
-                ),
-                Container(
-                  height: 30,
-                  width: 80,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(
+                  Container(
+                    height: 30,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: ColorConstant.grey,
+                        )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.remove,
+                              size: 15,
+                            )),
+                        Text("1"),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.add,
+                              size: 15,
+                            )),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: ColorConstant.grey,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Ristretto",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: ColorConstant.grey,
+                        )),
+                    child: Center(child: Text("One")),
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: ColorConstant.grey,
+                        )),
+                    child: Center(child: Text("Two")),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: ColorConstant.grey,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Onsite/TakeAway",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImageConstant.Onsite,
+                            ),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImageConstant.takeAway,
+                            ),
+                            fit: BoxFit.cover)),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: ColorConstant.grey,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Volume/ml",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImageConstant.ml250,
+                            ),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    height: 70,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImageConstant.ml350,
+                            ),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    height: 80,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              ImageConstant.ml450,
+                            ),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: ColorConstant.grey,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Prepare by a certain time today ?",
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+                  ),
+                  CupertinoSwitch(
+                    value: true,
+                    onChanged: (value) {},
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 36,
+                    width: 90,
+                    decoration: BoxDecoration(
                         color: ColorConstant.grey,
-                      )),
-                  child: Center(child: Text("One")),
-                ),
-                SizedBox(
-                  width: 25,
-                ),
-                Container(
-                  height: 30,
-                  width: 80,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(
-                        color: ColorConstant.grey,
-                      )),
-                  child: Center(child: Text("Two")),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: ColorConstant.grey,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Onsite/TakeAway",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 80,
-                ),
-                Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImageConstant.Onsite,
-                          ),
-                          fit: BoxFit.cover)),
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImageConstant.takeAway,
-                          ),
-                          fit: BoxFit.cover)),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: ColorConstant.grey,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Volume/ml",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 80,
-                ),
-                Container(
-                  height: 60,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImageConstant.ml250,
-                          ),
-                          fit: BoxFit.cover)),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Container(
-                  height: 70,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImageConstant.ml350,
-                          ),
-                          fit: BoxFit.cover)),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Container(
-                  height: 80,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                            ImageConstant.ml450,
-                          ),
-                          fit: BoxFit.cover)),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: ColorConstant.grey,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Prepare by a certain time today ?",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-                ),
-                CupertinoSwitch(
-                  value: true,
-                  onChanged: (value) {},
-                )
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: 36,
-                  width: 90,
-                  decoration: BoxDecoration(
-                      color: ColorConstant.grey,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "18 :",
-                        style: TextStyle(
-                            fontSize: 19,
-                            color: Color.fromARGB(255, 74, 74, 74)),
-                      ),
-                      Text(
-                        "10",
-                        style: TextStyle(fontSize: 19, color: Colors.black),
-                      ),
-                    ],
-                  )),
-                )
-              ],
-            )
-          ],
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "18 :",
+                          style: TextStyle(
+                              fontSize: 19,
+                              color: Color.fromARGB(255, 74, 74, 74)),
+                        ),
+                        Text(
+                          "10",
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ],
+                    )),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
