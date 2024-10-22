1d7c37b4e763affcbbd0208e998734540774968a
AthiraaAthi <athiraa2612@gmail.com>
2024-10-22 15:26:00 +0530

    added cart icon

--- lib/view/order_screen/coffee_lovers.dart
+++ lib/view/order_screen/coffee_lovers.dart
@@ -14,7 +14,17 @@ class CoffeeLoversScreen extends StatelessWidget {
           style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
         ),
         centerTitle: true,
-        actions: [],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+                child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 25,
+            )),
+          ),
+        ],
       ),
     );
   }
