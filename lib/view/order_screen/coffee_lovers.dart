59df2b1bb7c9cf11625f8b5068a30e13ba90f215
AthiraaAthi <athiraa2612@gmail.com>
2024-10-25 11:06:33 +0530

    added row for next option

--- lib/view/order_screen/coffee_lovers.dart
+++ lib/view/order_screen/coffee_lovers.dart
@@ -193,6 +193,9 @@ class _CoffeeLoversScreenState extends State<CoffeeLoversScreen> {
             Divider(
               color: ColorConstant.grey,
             ),
+            Row(
+              children: [],
+            )
           ],
         ),
       ),
