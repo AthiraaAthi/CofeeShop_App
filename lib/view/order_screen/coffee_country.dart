52186884f0d32d0ca0ca6beed2675c51be12981e
AthiraaAthi <athiraa2612@gmail.com>
2024-11-05 12:05:15 +0530

    added appbar

--- lib/view/order_screen/coffee_country.dart
+++ lib/view/order_screen/coffee_country.dart
@@ -5,6 +5,8 @@ class CoffeeCountryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
