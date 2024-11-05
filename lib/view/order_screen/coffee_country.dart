a6083291603c02943aedfc0c290f9d2366cc67a4
AthiraaAthi <athiraa2612@gmail.com>
2024-11-05 12:06:13 +0530

    added title and bg color with icon

--- lib/view/order_screen/coffee_country.dart
+++ lib/view/order_screen/coffee_country.dart
@@ -6,7 +6,25 @@ class CoffeeCountryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Coffee Lovers Assemblage",
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+                child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 25,
+            )),
+          ),
+        ],
+      ),
     );
   }
 }
