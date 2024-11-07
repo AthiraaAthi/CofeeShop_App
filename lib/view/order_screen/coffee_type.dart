15a1661162c2030e04087dc912ae2ec497df3659
AthiraaAthi <athiraa2612@gmail.com>
2024-11-07 08:49:38 +0530

    added cart icon along with the appbar

--- lib/view/order_screen/coffee_type.dart
+++ lib/view/order_screen/coffee_type.dart
@@ -13,6 +13,17 @@ class CoffeeTypeScreen extends StatelessWidget {
           style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
         ),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+                child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 25,
+            )),
+          ),
+        ],
       ),
     );
   }
