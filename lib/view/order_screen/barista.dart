e04e43d576093c32bbf10c2c05e71f89dc287e30
AthiraaAthi <athiraa2612@gmail.com>
2024-11-08 12:33:52 +0530

    added new screen for barista

---
+++ lib/view/order_screen/barista.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BaristaScreen extends StatelessWidget {
+  const BaristaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
