1b1b9a23dbd97277f3e0990bfed21edc51969273
AthiraaAthi <athiraa2612@gmail.com>
2024-11-08 12:34:23 +0530

    added appbar

--- lib/view/order_screen/barista.dart
+++ lib/view/order_screen/barista.dart
@@ -5,6 +5,8 @@ class BaristaScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
