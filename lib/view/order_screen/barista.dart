306ae176654513a9a4f7dcf16bf40a1317a04700
AthiraaAthi <athiraa2612@gmail.com>
2024-11-09 11:11:13 +0530

    added cart icon to the appbar

--- lib/view/order_screen/barista.dart
+++ lib/view/order_screen/barista.dart
@@ -13,6 +13,17 @@ class BaristaScreen extends StatelessWidget {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
         ),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+                child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 25,
+            )),
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
       body: Padding(
