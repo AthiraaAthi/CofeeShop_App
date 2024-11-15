8850810d3a8d2b0f34396ff9f786ac13d996d6eb
AthiraaAthi <athiraa2612@gmail.com>
2024-11-15 12:14:31 +0530

    changed slashes

--- lib/utils/image_constant/img_constant.dart
+++ lib/utils/image_constant/img_constant.dart
@@ -3,12 +3,12 @@ class ImageConstant {
   static String cart = "assets/images/cart.png";
   static String profile = "assets/images/profile.png";
   static String americano = "assets/images/Americano.png";
-  static String americano_noBg = "assets\images\Americano-noBg.png";
+  static String americano_noBg = "assets/images/Americano-noBg.png";
   static String cappuccino = "assets/images/cappucino.png";
-  static String cappuccino_noBg = "assets\images\cappucino-noBg.png";
+  static String cappuccino_noBg = "assets/images/cappucino-noBg.png";
   static String latte = "assets/images/Latte.png";
   static String flatWhite = "assets/images/Flat white.png";
-  static String flatWhite_noBg = "assets\images\Flat_white-noBg.png";
+  static String flatWhite_noBg = "assets/images/Flat_white-noBg.png";
   static String raf = "assets/images/raf.png";
   static String espresso = "assets/images/Espresso.png";
   static String qr = "assets/images/qr.png";
