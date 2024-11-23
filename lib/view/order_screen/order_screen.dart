32cd64c91aea92cc71fe7eb4ae4d3f679bd0f3cd
AthiraaAthi <athiraa2612@gmail.com>
2024-11-23 09:55:26 +0530

    adding text

--- lib/view/order_screen/order_screen.dart
+++ lib/view/order_screen/order_screen.dart
@@ -226,7 +226,7 @@ class _OrderScreenState extends State<OrderScreen> {
                     ],
                   ),
                   Column(
-                    children: [],
+                    children: [Text("")],
                   )
                 ],
               ),
