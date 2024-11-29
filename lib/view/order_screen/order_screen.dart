d513ec8a6aee2f25b380dc3a8a612d739a231d3e
AthiraaAthi <athiraa2612@gmail.com>
2024-11-29 09:22:19 +0530

    added spacer

--- lib/view/order_screen/order_screen.dart
+++ lib/view/order_screen/order_screen.dart
@@ -323,6 +323,7 @@ class _OrderScreenState extends State<OrderScreen> {
                   ),
                 ),
               ),
+              Spacer(),
               Row(
                 children: [
                   Text("Amount"),
