1cfe7be03736c5ea1c48b82e0b21d77a7989c336
AthiraaAthi <athiraa2612@gmail.com>
2024-12-17 11:22:47 +0530

    added image

--- lib/view/rewards_screen/rewards_screen.dart
+++ lib/view/rewards_screen/rewards_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cofee_ui/utils/color_constant/color_constant.dart';
+import 'package:cofee_ui/utils/image_constant/img_constant.dart';
 import 'package:flutter/material.dart';
 
 class RewardScreen extends StatelessWidget {
@@ -22,20 +23,48 @@ class RewardScreen extends StatelessWidget {
           children: [
             Container(
               padding: EdgeInsets.all(15),
-              height: 122,
+              height: 130,
               width: double.infinity,
               decoration: BoxDecoration(
                   color: ColorConstant.mainBlue,
                   borderRadius: BorderRadius.circular(10)),
-              child: Row(
+              child: Column(
                 children: [
-                  Text(
-                    "Loyality Card",
-                    style: TextStyle(color: ColorConstant.White),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Loyality Card",
+                        style: TextStyle(
+                          color: ColorConstant.White,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "4/8",
+                        style: TextStyle(
+                          color: ColorConstant.White,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: ColorConstant.White,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Image.asset(ImageConstant.coffee_reward),
                   )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
