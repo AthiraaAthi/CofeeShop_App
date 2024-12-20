d14267abee2c33856a2d7fc78f4e0321d902b9f9
AthiraaAthi <athiraa2612@gmail.com>
2024-12-20 18:17:28 +0530

    created redeem screen

--- lib/view/rewards_screen/redeem_screen.dart
+++ lib/view/rewards_screen/redeem_screen.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class RedeemScreen extends StatelessWidget {
+  const RedeemScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
