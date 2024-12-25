b2f888afc3a292bf393861a051b99ce5ffdbe187
AthiraaAthi <athiraa2612@gmail.com>
2024-12-25 18:42:05 +0530

    chnged bottom nav option from comingsson to my order

--- lib/view/bottom_nav/bottom_nav.dart
+++ lib/view/bottom_nav/bottom_nav.dart
@@ -55,7 +55,7 @@ class _BottomNavState extends State<BottomNav> {
                   BottomNavigationBarItem(
                       icon: Icon(Icons.card_giftcard), label: "Rewards"),
                   BottomNavigationBarItem(
-                      icon: Icon(Icons.shopping_cart), label: "ComingSoon"),
+                      icon: Icon(Icons.shopping_cart), label: "My Order"),
                 ]),
           ),
         ),
