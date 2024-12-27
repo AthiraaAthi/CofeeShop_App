4bdc58bc313d060661f7a1679bc338137e9fcfe2
AthiraaAthi <athiraa2612@gmail.com>
2024-12-27 17:23:47 +0530

    adding fontsize to coffee type

--- lib/view/my_order_screen/my_order_screen.dart
+++ lib/view/my_order_screen/my_order_screen.dart
@@ -71,7 +71,7 @@ class MyOrders extends StatelessWidget {
                           ),
                           Text(
                             "Americano",
-                            style: TextStyle(),
+                            style: TextStyle(fontSize: 15),
                           ),
                         ],
                       ),
