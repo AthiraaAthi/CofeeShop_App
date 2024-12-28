3e1313daa20ed99735f8126f65e6aeedfeb25704
AthiraaAthi <athiraa2612@gmail.com>
2024-12-28 16:49:10 +0530

    added the column to builder

--- lib/view/my_order_screen/my_order_screen.dart
+++ lib/view/my_order_screen/my_order_screen.dart
@@ -51,7 +51,80 @@ class _MyOrdersState extends State<MyOrders> {
         ),
         body: TabBarView(children: [
           ListView.builder(
-            itemBuilder: (context, index) {},
+            itemBuilder: (context, index) {
+              return Padding(
+                //1st tab
+                padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "24 June | 12:30 | by 18:10",
+                                style: TextStyle(color: ColorConstant.nameGrey),
+                              ),
+                              Text(
+                                "BYN 3.00",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.local_cafe_outlined,
+                                color: ColorConstant.mainBlue,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                "Americano",
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.location_on_outlined,
+                                color: ColorConstant.mainBlue,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                "Bradford BD1 1PR",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              );
+            },
           ),
           Column(
             //2ndtab
