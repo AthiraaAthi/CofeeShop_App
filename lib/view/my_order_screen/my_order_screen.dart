2d872bcf35fa45458aeadc3d3220664197f41eb8
AthiraaAthi <athiraa2612@gmail.com>
2024-12-28 16:46:55 +0530

    added second address to list

--- lib/view/my_order_screen/my_order_screen.dart
+++ lib/view/my_order_screen/my_order_screen.dart
@@ -21,6 +21,7 @@ class _MyOrdersState extends State<MyOrders> {
   ];
   List<String> address = [
     "Bradford BD1 1PR",
+    "Bradford BD1 1PR",
   ];
   @override
   Widget build(BuildContext context) {
