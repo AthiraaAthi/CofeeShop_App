7a71f4defd39aba7c743701115da951411f97a7c
AthiraaAthi <athiraa2612@gmail.com>
2024-12-31 18:58:16 +0530

    added alignment

--- lib/view/my_order_screen/my_order_screen.dart
+++ lib/view/my_order_screen/my_order_screen.dart
@@ -160,6 +160,7 @@ class _MyOrdersState extends State<MyOrders> {
                         ],
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Row(
                             children: [
